36f3ea3da111be575c099770cf936df33dca04a6
ananthakrishna-hs <h.s.ananthakrishna@gmail.com>
2022-12-25 12:52:24 +0530

    test: add tests for login and nav components

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -7,8 +7,16 @@ import { MemoryRouter } from 'react-router-dom';
 import App from 'App';
 import rootReducer from 'data/reducers/main';
 import middleware from 'data/middleware/main';
+import { Login } from 'components/Login/Login';
+import { DashboardNav } from 'components/DashboardNav/DashboardNav';
 
 const store = createStore(rootReducer, middleware);
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate
+}));
 
 describe('Test snapshots', () => {
   it('Test App snapshot', () => {
@@ -46,5 +54,60 @@ describe('Test Login screen', () => {
       </Provider>
     );
     expect(screen.getByText('Submit')).toHaveAttribute('disabled');
+  });
+
+  it('Alert is thrown when invalid credentials are entered', () => {
+    render(
+      <Login users={{'test': {'password': 'test'}}} dispatch={null} />
+    );
+
+    const id = screen.getByPlaceholderText('Enter your login ID');
+    const password = screen.getByPlaceholderText('Enter your password');
+    const submit = screen.getByText('Submit');
+
+    fireEvent.change(id, { target: { value: 'Test' } });
+    fireEvent.change(password, { target: { value: 'Test' } });
+
+    expect(submit).not.toBeDisabled();
+
+    fireEvent.click(submit);
+    
+    expect(screen.getByText('Invalid credentials')).toBeInTheDocument();
+  });
+
+  it('Successfull login will not have prompt', () => {
+    render(
+      <Login users={{'test': {'password': 'test'}}} dispatch={() => {}} />
+    );
+
+    const id = screen.getByPlaceholderText('Enter your login ID');
+    const password = screen.getByPlaceholderText('Enter your password');
+    const submit = screen.getByText('Submit');
+
+    fireEvent.change(id, { target: { value: 'test' } });
+    fireEvent.change(password, { target: { value: 'test' } });
+
+    expect(submit).not.toBeDisabled();
+
+    fireEvent.click(submit);
+    
+    expect(screen.queryByText('Invalid credentials')).not.toBeInTheDocument();
+  });
+});
+
+describe('Test nav component', () => {
+  it('Nav will have three tabs and user name is displayed', () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <DashboardNav username={'test'} dispatch={() => {}} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Leaderboard')).toBeInTheDocument();
+    expect(screen.getByText('New')).toBeInTheDocument();
+
+    expect(screen.getByText('test')).toBeInTheDocument();
   });
 });
